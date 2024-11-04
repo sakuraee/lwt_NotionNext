7f0001d6f80840367d4ae6ceb647ef60d47b3558
sakuraee <56988719+sakuraee@users.noreply.github.com>
2024-11-04 17:22:11 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -19,11 +19,8 @@ const CONFIG = {
 
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_2: '与生活思考',
+  HEO_HERO_TITLE_3: 'BLOG.SAKURAEE.COM'
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
